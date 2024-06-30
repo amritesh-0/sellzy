058fce5a0e2637c471f99aa036d08f397a05e5ad
Shivam Yadav <sy979514@gmail.com>
2024-06-30 14:12:12 +0530

    created backend server and handle user routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { registerUser, authUser, logoutUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', authUser);
+router.post('/logout', logoutUser);
+
+module.exports = router;
